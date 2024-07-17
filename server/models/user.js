910d3adf842f64fba8c80996308b2f9184ea4149
somayabusamor <somayab@ac.sce.ac.il>
2024-07-17 21:23:26 +0300

    BSPMS2431-1+BSPMS2431-6
    Sign Up and choose user role

--- server/models/user.js
+++ server/models/user.js
@@ -11,45 +11,29 @@ const userSchema = new mongoose.Schema({
   isAdmin: { type: Boolean, default: false },
   isStudent: { type: Boolean, default: false },
   isWorker: { type: Boolean, default: false },
-
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
-    expiresIn: "7d",
-  });
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin, isStudent: this.isStudent, isWorker: this.isWorker },
+    process.env.JWTPRIVATEKEY,
+    { expiresIn: "7d" }
+  );
   return token;
 };
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 
 const validate = (data) => {
   const schema = Joi.object({
-    firstName: Joi.string()
-      .required()
-      .label("First Name"),
-    lastName: Joi.string()
-      .required()
-      .label("Last Name"),
-    email: Joi.string()
-      .email()
-      .required()
-      .label("Email"),
-    password: passwordComplexity()
-      .required()
-      .label("Password"),
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+    isStudent: Joi.boolean().label("isStudent"),
+    isWorker: Joi.boolean().label("isWorker"),
   });
   return schema.validate(data);
 };
-////////
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  const hash = await bcrypt.hash(this.password, Number(bcryptSalt));
-  this.password = hash;
-  next();
-});
-/////
+
 module.exports = { User, validate };
-//////////
